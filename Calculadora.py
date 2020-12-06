4cfa7e1915ccdf1ef8f6a0ff6626769342283763
Abdiel Athayde <46298069+abdieldeathayde@users.noreply.github.com>
2020-12-06 01:38:56 -0300

    update file

--- Calculadora.py
+++ Calculadora.py
@@ -1,42 +1,71 @@
+from sys import exit
+
 def Calculadora():
+  
 
-  num1 = input ("Digite o primeiro numero: ");
-  num1 = int (num1);
-
-  num2 = input ("Digite o segundo numero: ");
-  num2 = int (num2);
-
-  operacao = input ("-----Escolha uma operação matemática ----\n\n '+' soma \n '-' subtracao \n '*' multiplicacao \n '/' divisao \n 'pow' potencia\n");
-  operacao = str (operacao); 
+  operacao = 1
 
 
-  if operacao == '+':
-    soma = num1 + num2;
+  while operacao > 0:
 
-    print(soma);
+    print("Digite o primeiro numero: ");
+    num1 = int(input())
+    
 
-  elif operacao == '-':
-    sub = num1 - num2;
+    print("Digite o segundo numero: ");
+    num2 = int(input())
+ 
 
-    print (sub);
+    print ("-----Escolha uma operação matemática ----\n\n 1 - soma \n 2 - subtracao \n 3 - multiplicacao \n 4 - divisao \n 5 - potencia\n '0' Sair");
+    operacao = int(input())
+    opcoes(operacao, num1, num2)
+
+    
+   
 
 
-  elif operacao == "*":
-    mult = num1 * num2;
+def opcoes(operacao, num1, num2):
+  if operacao == 1:
+    somar(num1, num2)
 
-    print(mult);
+  elif operacao == 2:
+    subtrair(num1, num2)
 
-  elif operacao == "/":
-    div = num1 / num2;
 
-    print(div);
+  elif operacao == 3:
+    multiplicar(num1, num2)
 
-  elif operacao == "pow":
-   potencia = pow(num1,num2);
+  elif operacao == 4:
+    dividir(num1, num2)
 
-   print(potencia);
+  elif operacao == 5:
+    elevar(num1, num2)
+
+  elif operacao == 0:
+    exit()
 
   else:
-    print("Erro, tente novamente");
+    print("Erro, tente novamente"); 
+
+def somar(num1, num2):
+  soma = num1 + num2;
+  print(soma);
+
+def subtrair(num1, num2):
+  sub = num1 - num2;
+  print (sub);
+
+def multiĺicar(num1, num2):
+  mult = num1 * num2;
+  print(mult);
+
+def dividir(num1, num2):
+  div = num1 / num2;
+  print(div);
+
+def elevar(num1, num2):
+  potencia = pow(num1,num2);
+  print(potencia);
 
 Calculadora();
+
